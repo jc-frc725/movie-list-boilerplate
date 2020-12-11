ad8f56af75601378c65314b8b2526ae1263b1cca
Justin Chow <hrll71990@gmail.com>
2020-12-10 19:32:43 -0800

    added SearchBar component, can read user input value, slight css change

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieList from './MovieList.jsx';
+import SearchBar from './SearchBar.jsx';
 
 // const App = (props) => (
 //   <div>Hello World!</div>
@@ -24,6 +25,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello World!</h1>
+        <SearchBar />
         <MovieList movies={this.state.movies}/>
       </div>
     );
