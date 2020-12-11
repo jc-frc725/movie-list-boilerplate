0f59f4f745bba442df3875aca356c7a96efa7c8e
Justin Chow <hrll71990@gmail.com>
2020-12-10 22:41:20 -0800

    implemented searchMovie function, currently only works with search term matching exactly matching titles

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -18,24 +18,31 @@ class App extends React.Component {
     super(props)
     this.state = {
       movies: [],
-      searchQuery: ''
+      searchedMovies: []
+      //search: ''
     }
     this.searchMovie = this.searchMovie.bind(this);
   }
 
   componentDidMount() {
-    setTimeout(() => this.setState({movies}), 1000);
+    // initialize movie state and searched movie state
+    setTimeout(() => this.setState({movies: movies, searchedMovies: movies}), 1000);
   }
 
   searchMovie(query) {
-    //console.log(query);
-    const searchedMovies = [];
-    movies.forEach(movie => {
-      if (movie.title.indexOf(query) !== -1) {
-        searchedMovies.push(movie);
-      }
+    console.log(query);
+    //this.setState({search: query})
+    // filter this.movies based on query but can't actually mutate it,
+    // or else actual movies state is changed forever, movie objects can be lost
+
+    // capture current searchedMovie state based on full data set
+    var searchedMovies = this.state.movies;
+    // filter out based on user search 
+    searchedMovies = searchedMovies.filter((movie) => {
+      return (movie.title.indexOf(query) !== -1)
     });
-    this.setState({movies: searchedMovies});
+    // update state
+    this.setState({searchedMovies});
   }
 
   render() {
@@ -43,7 +50,7 @@ class App extends React.Component {
       <div>
         <h1>Hello World!</h1>
         <SearchBar searchMovie={this.searchMovie}/>
-        <MovieList movies={this.state.movies}/>
+        <MovieList movies={this.state.searchedMovies}/>
       </div>
     );
   }
