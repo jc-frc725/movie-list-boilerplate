6f9880ea3e2baf0ee64ca77b069ba367e6bdcfba
Justin Chow <hrll71990@gmail.com>
2020-12-22 01:14:40 -0800

    handles 'no movie found' when no results from search

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -23,9 +23,10 @@ class App extends React.Component {
     this.state = {
       movies: [],
       searchedMovies: [],
-      //noMovies: function() {console.log("no movie by that name found")}
+      noMoviesFound: false
     }
     this.searchMovie = this.searchMovie.bind(this);
+    //this.handleNoMovies = this.handleNoMovies.bind(this);
   }
 
   componentDidMount() {
@@ -36,7 +37,6 @@ class App extends React.Component {
   searchMovie(query) {
     // filter this.movies based on query but can't actually mutate it,
     // or else actual movies state is changed forever, movie objects can be lost
-
     // capture current searchedMovie state based on full data set
     var searchedMovies = this.state.movies;
     // filter out based on user search 
@@ -45,19 +45,35 @@ class App extends React.Component {
       query = query.toLowerCase();
       return (movie.title.toLowerCase().indexOf(query) !== -1)
     });
-    // update state
-    this.setState({searchedMovies});
+    //this.setState({searchedMovies});
     
-    if (this.state.searchedMovies.length === 0) {
-      console.log("no movie by that name found");
+    if (searchedMovies.length === 0) {
+      this.setState({searchedMovies, noMoviesFound: true});
+    } else {
+      this.setState({searchedMovies, noMoviesFound: false});
     }
+
   }
 
+  // handle case where no movies are found after search
+  // handleNoMovies() {
+  //   if (this.state.searchedMovies.length === 0) {
+  //     this.setState({noMoviesFound: true});
+  //   }
+  // }
+
   render() {
+    // style conditional, updates on user interaction
+    const style = {
+      // put css styles here, they will change by updating state
+      display: this.state.noMoviesFound ? 'block' : 'none'
+    }
+
     return (
       <div>
         <h1>Movie List</h1>
         <SearchBar searchMovie={this.searchMovie}/>
+        <p style={style}>No movie by that name found.</p>
         <MovieList movies={this.state.searchedMovies}/>
       </div>
     );
